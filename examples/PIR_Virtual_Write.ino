c97be73ddf6b2360cff9b26149c1d5786abce5f3
Embedded Team <petalsautomationembedded@gmail.com>
2026-02-25 17:31:10 +0530

    Create PIR_Virtual_Write.ino

---
+++ examples/PIR_Virtual_Write.ino
@@ -0,0 +1,34 @@
+/*************************************************************
+
+  You’ll need:
+   - Spell IoT app (download from App Store or Google Play)
+   - ESP32/ESP8266 board
+   - Decide how to connect to SPELLIOT
+     (Ethernet, Wi-Fi )
+  Code Developed By : M.Karthickraja
+  Date : 25-02-2026
+  *************************************************************/
+
+/* Fill-in information from SPELLIoT Device Info here */
+#include <Spell_IoT.h>
+
+#define WIFI_SSID "*******"
+#define WIFI_PASSWORD "**********"
+#define DEVICE_TOKEN "***********"
+
+#define PIR 5
+
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(PIR, INPUT);
+  Spell_iot.begin(WIFI_SSID, WIFI_PASSWORD, DEVICE_TOKEN);
+}
+
+void loop()
+{
+ Spell_iot.loop();
+ int pirState = digitalRead(PIR) ;
+ Spell_iot.write("V7", pirState);
+ delay(10);
+}
