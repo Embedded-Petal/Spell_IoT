42d46a01a8007f5fa9af500c76e7cfe31f939231
Embedded Team <petalsautomationembedded@gmail.com>
2026-02-25 18:15:01 +0530

    Create Touchbutton_Virtual_Write.ino

---
+++ examples/Touchbutton_Virtual_Write/Touchbutton_Virtual_Write.ino
@@ -0,0 +1,55 @@
+/*************************************************************
+
+  You’ll need:
+   - Spell IoT app (download from App Store or Google Play)
+   - ESP32/ESP8266 board
+   - Decide how to connect to SPELLIOT
+     (Ethernet, Wi-Fi )
+  Code Developed By : M.Karthickraja
+  Date : 25-02-2026
+  *************************************************************/
+
+/* Fill-in information from SPELLIoT Device Info here */
+
+#include <Spell_IoT.h>
+
+#define WIFI_SSID "***********"
+#define WIFI_PASSWORD "**********"
+#define DEVICE_TOKEN "******************"
+
+#define Button4 36
+#define Button3 39
+#define Button2 34
+#define Button1 35
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(Button1, INPUT);
+  pinMode(Button2, INPUT);
+  pinMode(Button3, INPUT);
+  pinMode(Button4, INPUT);
+  Spelliot.begin(WIFI_SSID, WIFI_PASSWORD, DEVICE_TOKEN);
+}
+
+void loop() {
+  Spell_iot.loop();
+  if (digitalRead(Button1) == 1)
+    Spell_iot.write("V0", 1);
+  else
+    Spell_iot.write("V0", 0);
+
+  if (digitalRead(Button2) == 1)
+    Spell_iot.write("V1", 1);
+  else
+    Spell_iot.write("V1", 0);
+
+  if (digitalRead(Button3) == 1)
+    Spell__iot.write("V2", 1);
+  else
+    Spell_iot.write("V2", 0);
+
+  if (digitalRead(Button4) == 1)
+    Spell_iot.write("V3", 1);
+  else
+    Spell_iot.write("V3", 0);
+}
