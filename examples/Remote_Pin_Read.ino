84754fa4e622ddc6cfbce47e8e9aaa80a2239091
Embedded Team <petalsautomationembedded@gmail.com>
2026-02-25 17:20:56 +0530

    Create Remote_Pin_Read.ino

---
+++ examples/Remote_Pin_Read.ino
@@ -0,0 +1,38 @@
+/*************************************************************
+
+  You’ll need:
+   - Spell IoT app (download from App Store or Google Play)
+   - ESP32/ESP8266 board
+   - Decide how to connect to SPELLIOT
+     (Ethernet, Wi-Fi )
+
+  *************************************************************/
+
+/* Fill-in information from SPELLIoT Device Info here */
+
+#include <Spell_IoT.h>
+
+#define WIFI_SSID     "**************"
+#define WIFI_PASSWORD  "**************"
+#define DEVICE_TOKEN  "*******************"
+
+#define LED1 13
+int LEDState1;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED1, OUTPUT);
+  digitalWrite(LED1, LOW);
+  Spell_iot.begin(WIFI_SSID, WIFI_PASSWORD, DEVICE_TOKEN);
+  Spell_iot.registerPin("R0", [](String v) {
+    LEDState1 = v.toInt();
+  });
+}
+
+void loop() {
+  Spell_iot.loop();
+  if (LEDState1 == 1 ) 
+    digitalWrite(LED1, HIGH);
+  else
+    digitalWrite(LED1, LOW);
+}  
