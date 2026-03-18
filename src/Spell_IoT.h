ea9c088da4d35ffcb09a5a139c1c1585778d67fa
Embedded Team <petalsautomationembedded@gmail.com>
2026-03-18 18:20:34 +0530

    Update Spell_IoT.h

--- src/Spell_IoT.h
+++ src/Spell_IoT.h
@@ -1,6 +1,25 @@
 #ifndef SPELL_IOT_H
 #define SPELL_IOT_H
 
+#if !defined(SKYLINK_FIRMWARE_VERSION)
+    #define SKYLINK_FIRMWARE_VERSION  "0.0.0"
+#endif
+
+#if defined(SPELLIOT_UPDATE)
+  #define UPDATE_SKYLINK
+#endif
+
+#ifdef SPELLIOT_LED_DISPLAY
+#define PETALLED_MATRIX
+#include "Spelliot_Ledmatrix.hpp"
+#endif
+
+#ifdef SPELLIOT_STEPPER_MOTOR
+#include "StepperMotor.h"
+#endif
+
 #include "iotCloud.h"
+#include "iotCloud.hpp"
 
 #endif
+
