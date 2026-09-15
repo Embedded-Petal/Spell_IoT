2e1372b3515f9bb51ae81fec0e95af473990a812
Embedded Team <petalsautomationembedded@gmail.com>
2026-09-15 10:21:36 +0530

    Update LEDMatrix_Virtual_Read.ino

--- examples/program with cpp/LEDMatrix_Virtual_Read/LEDMatrix_Virtual_Read.ino
+++ examples/program with cpp/LEDMatrix_Virtual_Read/LEDMatrix_Virtual_Read.ino
@@ -11,9 +11,8 @@
   *************************************************************/
 
 /* Fill-in information from SPELLIoT Device Info here */
+#define SPELLIOT_LED_DISPLAY
 #include <Spell_IoT.h>
-
-#include <Spelliot_Ledmatrix.h>
 
 #define WIFI_SSID     "**********"
 #define WIFI_PASSWORD  "***********"
@@ -65,7 +64,8 @@ void setup()
 
 void loop()
 {
-displayPrint();
+  Spell_iot.loop();
+  displayPrint();
 
 }
 
